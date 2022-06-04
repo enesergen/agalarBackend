7d888274f9eab6667e57be09d8630dbd1c1cd2e1
ysumut <umut_aslan95@hotmail.com>
2022-06-05 00:08:13 +0300

    dotenv added

--- db.js
+++ db.js
@@ -1,10 +1,12 @@
 const Pool = require("pg").Pool;
+require('dotenv').config();
 
-const pool = new Pool({
-    user: "postgres",
-    password: "123",
-    database: "agalar",
-    host: "localhost",
-    port: 5432
+const db = new Pool({
+    user: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+    database: process.env.PG_DATABASE,
+    host: process.env.PG_HOST,
+    port: process.env.PG_PORT
 });
-module.exports = pool;
+
+module.exports = db;
